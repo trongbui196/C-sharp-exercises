b24605c14f54f8a37f6f79a2c09cb3850d45b298
trongbui196 <39149120+trongbui196@users.noreply.github.com>
2024-02-22 17:49:37 +0700

    Add files via upload

---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{   static void Main(string[] args)
+    {
+        int[] nums = [1, 2, 3];
+        Console.WriteLine(nums.Length);
+        Console.WriteLine(nums[2]);
+
+    }
+    //public static int[] TwoSum(int[] nums, int target)
+    //{   
+    //    int[] result = {0,1};
+    //    for(int i = 0; i < nums.Length; i++)
+    //    {
+    //        for (int j = 1; j < nums.Length-1; j++) 
+    //        {
+    //            if (nums[i] + nums[j] == target)
+    //            {
+    //                result[0] = i;
+    //                result[1] = j;
+    //                return result;
+    //            }
+    //        }
+    //    }
+    //    return result;
+
+    //}
+}
